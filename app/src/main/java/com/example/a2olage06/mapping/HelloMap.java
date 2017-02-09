15cc566d320c7a403b15daf44bd9d3ee9594101f
2olage06 <2olage06@solent.ac.uk>
2017-02-09 10:50:14 +0000

    Second activity added with 2 menu options(cycling and normal view)

--- app/src/main/java/com/example/a2olage06/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2olage06/mapping/HelloMap.java
@@ -1,6 +1,7 @@
 package com.example.a2olage06.mapping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.IntegerRes;
@@ -10,9 +11,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HelloMap extends Activity implements View.OnClickListener
 {
@@ -49,4 +56,46 @@ public class HelloMap extends Activity implements View.OnClickListener
         mv.getController().setCenter(new GeoPoint(latitude, longitude));
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            // react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 }
